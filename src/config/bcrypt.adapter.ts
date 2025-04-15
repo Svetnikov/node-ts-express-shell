e087f902309e6d87d9ef93ef72d63bf41ba47731
Svetnikov <antonio.2409@outlook.com>
2025-04-15 08:51:06 -0400

    Agregar soporte para autenticación JWT; incluir dependencias de jsonwebtoken y tipos, y refactorizar bcrypt adapter

--- src/config/bcrypt.adapter.ts
+++ src/config/bcrypt.adapter.ts
@@ -1,7 +1,16 @@
-import bcrypt from "bcryptjs";
+import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+
+
 
 export const bcryptAdapter = {
-    hash: (password: string) => {
-        const salt = gen
-    }
+
+  hash: (password: string) => {
+    const salt = genSaltSync();
+    return hashSync(password, salt)
+  },
+
+  compare: (password:string, hashed: string) => {
+    return compareSync(password, hashed);
+  }
+
 }
